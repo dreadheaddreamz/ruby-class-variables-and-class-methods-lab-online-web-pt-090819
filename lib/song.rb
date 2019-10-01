bc7418d574a87af14eceaee3794bd8f2dd716bbf
James Batsell <jamesbatsell05@gmail.com>
2019-10-01 21:09:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   
   def self.genre_count
     genre_count = {}
-    @@genres.each do |gen|
+    @@genres.each do |genre|
       if genre_count[genre]
         genre_count[genre] += 1 
         else
