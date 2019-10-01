8df53e4496a11ca9b3da3800fb415b7b2a758d4b
James Batsell <jamesbatsell05@gmail.com>
2019-10-01 20:57:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,29 @@ class Song
     @@artists.uniq
   end
   
+    def self.genre_count
+    new = {}
+    @@genres.each do |gen|
+      if !(new.keys.include?(gen))
+        new[gen] = 1 
+      elsif new.keys.include?(gen)
+        new[gen] += 1 
+      end
+    end
+    new
+  end
   
+  def self.artist_count
+    new = {}
+    @@artists.each do |art|
+      if !(new.keys.include?(art))
+        new[art] = 1 
+      elsif new.keys.include?(art)
+        new[art] += 1
+      end
+    end
+    new
+  end
   
   
   
