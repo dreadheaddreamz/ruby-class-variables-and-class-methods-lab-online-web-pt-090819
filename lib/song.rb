7fcd113e29379f573bb4bdf9260ec28f101552aa
James Batsell <jamesbatsell05@gmail.com>
2019-10-01 20:41:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,15 @@ class Song
   @@song_count = 0
   
   def initialize
+    @@artists
     @@song_count += 1
   end
   
   def self.count
     @@song_count
+  end
+  
+  def self.split
+    @@artists
   end
 end
